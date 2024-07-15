458f19c82cbf0058a9cfcb1c420c899bfd682ef1
Saurabh Kavhar <skavhar111@gmail.com>
2024-07-15 15:32:32 +0530

    fixed NaN invalid JSON payload

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ from google.cloud import translate_v2 as translate
 # Set the environment variable for authentication
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service-account-file.json'
 
+# Initialize Google Cloud Translation client
+client = translate.Client()
+
+# Function to translate text, handling empty values
+def translate_text_safe(text, source_language, target_language):
+    if pd.isna(text):  # Check if the value is NaN
+        return ''
+    else:
+        translation = client.translate(text, source_language=source_language, target_language=target_language)
+        return translation['translatedText']
+
 # Function to translate Excel file
 def translate_excel(source_language, target_languages, excel_path):
-    # Initialize the translation client
-    client = translate.Client()
-
     # Read Excel file
     excel_data = pd.read_excel(excel_path, sheet_name=None)  # Read all sheets into a dictionary of DataFrames
 
@@ -19,7 +27,7 @@ def translate_excel(source_language, target_languages, excel_path):
         
         # Translate each column in the DataFrame
         for column in df.columns:
-            df[column] = df[column].apply(lambda x: client.translate(x, target_language=target_languages[0], source_language=source_language)['translatedText'])
+            df[column] = df[column].apply(lambda x: translate_text_safe(str(x), source_language, target_languages[0]))
 
         # Save translated DataFrame to a new Excel file
         output_excel = f"translated_{sheet_name}.xlsx"
